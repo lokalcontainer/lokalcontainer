3a0279770fb8bdf0b1d7e5092d05e981e6bcad32
Taufik Oktama <oktamataufik@gmail.com>
2022-01-07 21:54:40 +0700

    `feat` getInitialProps()

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,11 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript, DocumentContext } from "next/document";
 
 export default class MyDocument extends Document {
+    static async getInitialProps(ctx: DocumentContext) {
+        const initialProps = await Document.getInitialProps(ctx);
+        return { ...initialProps };
+    }
+
     render(): JSX.Element {
         return (
             <Html lang="en">
