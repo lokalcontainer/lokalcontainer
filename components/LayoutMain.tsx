51c7bd0db649f1fc311f55e31fbce1d59f5543c6
Taufik Oktama <oktamataufik@gmail.com>
2022-01-10 19:23:17 +0700

    `feat` Manifest

--- components/LayoutMain.tsx
+++ components/LayoutMain.tsx
@@ -1,4 +1,8 @@
 import type { FC, CSSProperties } from "react";
+import { useRouter } from "next/router";
+import NextHead from "next/head";
+
+import { SITE_DATA } from "libs/site-data.constants";
 
 type LayoutMainProps = {
     style?: CSSProperties;
@@ -6,9 +10,28 @@ type LayoutMainProps = {
 
 export const LayoutMain: FC<LayoutMainProps> = (props) => {
     const { children, style } = props;
+    const { asPath } = useRouter();
+    const NEW_URL = `${SITE_DATA.url}${asPath}`;
 
     return (
         <>
+            <NextHead>
+                <title>{SITE_DATA.title}</title>
+                <meta name="title" content={SITE_DATA.title} />
+                <meta name="description" content={SITE_DATA.description} />
+
+                <meta property="og:type" content={SITE_DATA.type} />
+                <meta property="og:url" content={NEW_URL} />
+                <meta property="og:title" content={SITE_DATA.title} />
+                <meta property="og:description" content={SITE_DATA.description} />
+                <meta property="og:image" content={SITE_DATA.image} />
+
+                <meta property="twitter:card" content="summary_large_image" />
+                <meta property="twitter:url" content={NEW_URL} />
+                <meta property="twitter:title" content={SITE_DATA.title} />
+                <meta property="twitter:description" content={SITE_DATA.description} />
+                <meta property="twitter:image" content={SITE_DATA.image} />
+            </NextHead>
             <main
                 style={{
                     position: "relative",
@@ -18,6 +41,7 @@ export const LayoutMain: FC<LayoutMainProps> = (props) => {
                     marginBottom: "var(--footer-height)",
                     backgroundColor: "var(--accents-1)",
                     zIndex: 10,
+                    // boxShadow: "0 0 1em -0.5em rgba(0, 0, 0, 1)",
                     ...style
                 }}
             >
