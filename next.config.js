653ba9211d92aebb4e239b1986e632bd3b78e263
Taufik Oktama <oktamataufik@gmail.com>
2022-01-11 19:12:08 +0700

    `feat` Use submodule `lokalcontainer/fonts`

--- next.config.js
+++ next.config.js
@@ -1,12 +1,4 @@
 module.exports = {
     reactStrictMode: true,
-    poweredByHeader: false,
-    async rewrites() {
-        return [
-            {
-                source: "/fonts/:path*",
-                destination: "https://github.com/lokalcontainer/fonts/blob/main/:path*"
-            }
-        ];
-    }
+    poweredByHeader: false
 };
