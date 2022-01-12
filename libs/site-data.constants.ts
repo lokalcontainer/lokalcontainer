593d60b91dd8b72d61cd22502e1719ba0d336e97
Taufik Oktama <oktamataufik@gmail.com>
2022-01-12 10:24:41 +0700

    `feat` Ignore directory and change global `site title`

--- libs/site-data.constants.ts
+++ libs/site-data.constants.ts
@@ -1,7 +1,7 @@
 const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL!;
 
 export const SITE_DATA = {
-    title: "Lokal Container",
+    title: "Lokal Container - An Open Source Font Publisher",
     description:
         "Open source typographic platform initiated by young people from Bandung, Indonesia",
     image: `${SITE_URL}/images/cards/og_image.jpg`,
