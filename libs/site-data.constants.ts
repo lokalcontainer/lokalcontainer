b53aac685ca4430243c11ccc6bef841f10a153c4
Taufik Oktama <oktamataufik@gmail.com>
2022-01-12 10:10:33 +0700

    `feat` Use `env vars`

--- libs/site-data.constants.ts
+++ libs/site-data.constants.ts
@@ -1,12 +1,10 @@
-export const SITE_TITLE = "Lokal Container";
-export const SITE_DESCRIPTION =
-    "Open source typographic platform initiated by young people from Bandung, Indonesia";
+const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL!;
 
 export const SITE_DATA = {
     title: "Lokal Container",
     description:
         "Open source typographic platform initiated by young people from Bandung, Indonesia",
-    image: "https://lokalcontainer.org/images/cards/og_image.jpg",
+    image: `${SITE_URL}/images/cards/og_image.jpg`,
     type: "website",
-    url: "https://lokalcontainer.org"
+    url: SITE_URL
 };
