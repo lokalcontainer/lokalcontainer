a39fe74f6f6a8f66107b4e5193ffcdfc44f9d630
Taufik Oktama <oktamataufik@gmail.com>
2022-02-11 12:43:58 +0700

    `feat` Rewrite static asset to `server` application

--- next.config.js
+++ next.config.js
@@ -3,10 +3,10 @@ const API_URL = process.env.NEXT_PUBLIC_API_URL;
 module.exports = {
     reactStrictMode: true,
     poweredByHeader: false,
-    images: {
-        domains: ["flagcdn.com"]
-    },
     async rewrites() {
-        return [{ source: "/api/v1/:path*", destination: `${API_URL}/v1/:path*` }];
+        return [
+            { source: "/api/v1/:path*", destination: `${API_URL}/v1/:path*` },
+            { source: "/static/:path*", destination: `${API_URL}/static/:path*` }
+        ];
     }
 };
