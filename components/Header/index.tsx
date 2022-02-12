8f1a2be595cad2af8ed0d12dc5da00bde3774acf
Taufik Oktama <oktamataufik@gmail.com>
2022-02-12 22:04:30 +0700

    `fix` Leave logo alone

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,14 +1,15 @@
 import styles from "styles/header.module.scss";
-import { FC, useEffect, useRef, useState } from "react";
-import { useTheme } from "next-themes";
-import { AnimatePresence, motion } from "framer-motion";
-import { useRouter } from "next/router";
+// import type { FC } from "react";
+// import { useEffect, useRef, useState } from "react";
+// import { useTheme } from "next-themes";
+// import { AnimatePresence, motion } from "framer-motion";
+// import { useRouter } from "next/router";
 import NextLink from "next/link";
 
-import { STATIC_MENU } from "libs/menu.constants";
-import useOnClickOutside from "hooks/use-on-click-outside";
-import Logo from "components/Logo";
-import { useSession } from "components/Context/ContextSession";
+// import { STATIC_MENU } from "libs/menu.constants";
+// import useOnClickOutside from "hooks/use-on-click-outside";
+// import Logo from "components/Logo";
+// import { useSession } from "components/Context/ContextSession";
 
 // const MenuIcon: FC = ({ children }) => {
 //     return (
@@ -27,125 +28,125 @@ import { useSession } from "components/Context/ContextSession";
 //     );
 // };
 
-const MenuButton: FC<{ onClick: (e: any) => void }> = ({ children, onClick }) => {
-    return (
-        <button
-            onClick={(e) => onClick(e)}
-            style={{
-                appearance: "none",
-                background: "none",
-                border: "none",
-                fontFamily: "inherit",
-                fontSize: "inherit",
-                color: "inherit",
-                cursor: "pointer",
-                padding: 0,
-                margin: 0,
-                // height: "100%",
-                // aspectRatio: "1/1",
-                display: "inline-flex",
-                alignItems: "center",
-                justifyContent: "center",
-                zIndex: 2
-            }}
-        >
-            {children}
-        </button>
-    );
-};
+// const MenuButton: FC<{ onClick: (e: any) => void }> = ({ children, onClick }) => {
+//     return (
+//         <button
+//             onClick={(e) => onClick(e)}
+//             style={{
+//                 appearance: "none",
+//                 background: "none",
+//                 border: "none",
+//                 fontFamily: "inherit",
+//                 fontSize: "inherit",
+//                 color: "inherit",
+//                 cursor: "pointer",
+//                 padding: 0,
+//                 margin: 0,
+//                 // height: "100%",
+//                 // aspectRatio: "1/1",
+//                 display: "inline-flex",
+//                 alignItems: "center",
+//                 justifyContent: "center",
+//                 zIndex: 2
+//             }}
+//         >
+//             {children}
+//         </button>
+//     );
+// };
 
-const MainMenu = () => {
-    const [menu, setMenu] = useState(false);
-    const refMenu = useRef<HTMLLIElement>(null);
-    useOnClickOutside(refMenu, () => setMenu(false));
+// const MainMenu = () => {
+//     const [menu, setMenu] = useState(false);
+//     const refMenu = useRef<HTMLLIElement>(null);
+//     useOnClickOutside(refMenu, () => setMenu(false));
 
-    const { events } = useRouter();
-    useEffect(() => {
-        const handler = () => setMenu(false);
-        events.on("routeChangeComplete", handler);
-        return () => events.off("routeChangeComplete", handler);
-    }, [events]);
+//     const { events } = useRouter();
+//     useEffect(() => {
+//         const handler = () => setMenu(false);
+//         events.on("routeChangeComplete", handler);
+//         return () => events.off("routeChangeComplete", handler);
+//     }, [events]);
 
-    return (
-        <li ref={refMenu} data-menu="text">
-            <AnimatePresence initial={true} exitBeforeEnter>
-                {menu && (
-                    <motion.span
-                        initial={{ opacity: 0, width: "0em" }}
-                        animate={{ opacity: 1, width: "10em" }}
-                        exit={{ opacity: 0, width: "0em" }}
-                        style={{ position: "relative", width: "100%" }}
-                    >
-                        <span
-                            style={{
-                                padding: "0 var(--grid-gap)",
-                                display: "block",
-                                overflow: "hidden",
-                                whiteSpace: "nowrap",
-                                textOverflow: "ellipsis"
-                            }}
-                        >
-                            Menu
-                        </span>
+//     return (
+//         <li ref={refMenu} data-menu="text">
+//             <AnimatePresence initial={true} exitBeforeEnter>
+//                 {menu && (
+//                     <motion.span
+//                         initial={{ opacity: 0, width: "0em" }}
+//                         animate={{ opacity: 1, width: "10em" }}
+//                         exit={{ opacity: 0, width: "0em" }}
+//                         style={{ position: "relative", width: "100%" }}
+//                     >
+//                         <span
+//                             style={{
+//                                 padding: "0 var(--grid-gap)",
+//                                 display: "block",
+//                                 overflow: "hidden",
+//                                 whiteSpace: "nowrap",
+//                                 textOverflow: "ellipsis"
+//                             }}
+//                         >
+//                             Menu
+//                         </span>
 
-                        <ul
-                            style={{
-                                position: "absolute",
-                                left: -1,
-                                bottom: 0,
-                                transform: "translateY(calc(var(--grid-gap) + 100% + 2px))",
+//                         <ul
+//                             style={{
+//                                 position: "absolute",
+//                                 left: -1,
+//                                 bottom: 0,
+//                                 transform: "translateY(calc(var(--grid-gap) + 100% + 2px))",
 
-                                flexDirection: "column",
-                                padding: "calc(var(--grid-gap) * 2) var(--grid-gap)",
-                                margin: 0,
-                                display: "flex",
-                                // gap: "calc(var(--grid-gap) / 2)",
-                                backgroundColor: "var(--accents-1)",
-                                width: "calc(100% + calc(var(--grid-gap) * 3))",
+//                                 flexDirection: "column",
+//                                 padding: "calc(var(--grid-gap) * 2) var(--grid-gap)",
+//                                 margin: 0,
+//                                 display: "flex",
+//                                 // gap: "calc(var(--grid-gap) / 2)",
+//                                 backgroundColor: "var(--accents-1)",
+//                                 width: "calc(100% + calc(var(--grid-gap) * 3))",
 
-                                listStyle: "none",
-                                boxShadow: "0 0 1em 0 var(--accents-12)",
-                                border: "1px solid",
-                                borderRadius: "calc(var(--grid-gap) / 3)"
-                            }}
-                        >
-                            {STATIC_MENU.map((item, i) => (
-                                <li
-                                    key={i}
-                                    style={{
-                                        borderBottom: "1px solid",
-                                        borderTop: "1px solid",
-                                        marginBottom: -1
-                                    }}
-                                >
-                                    <NextLink {...item.link}>
-                                        <a>
-                                            <span>{item.label}</span>
-                                        </a>
-                                    </NextLink>
-                                </li>
-                            ))}
-                        </ul>
-                    </motion.span>
-                )}
-            </AnimatePresence>
+//                                 listStyle: "none",
+//                                 boxShadow: "0 0 1em 0 var(--accents-12)",
+//                                 border: "1px solid",
+//                                 borderRadius: "calc(var(--grid-gap) / 3)"
+//                             }}
+//                         >
+//                             {STATIC_MENU.map((item, i) => (
+//                                 <li
+//                                     key={i}
+//                                     style={{
+//                                         borderBottom: "1px solid",
+//                                         borderTop: "1px solid",
+//                                         marginBottom: -1
+//                                     }}
+//                                 >
+//                                     <NextLink {...item.link}>
+//                                         <a>
+//                                             <span>{item.label}</span>
+//                                         </a>
+//                                     </NextLink>
+//                                 </li>
+//                             ))}
+//                         </ul>
+//                     </motion.span>
+//                 )}
+//             </AnimatePresence>
 
-            <MenuButton onClick={() => setMenu((prev) => !prev)}>
-                <span>Menu</span>
-                {/* <MenuIcon>M</MenuIcon> */}
-            </MenuButton>
-        </li>
-    );
-};
+//             <MenuButton onClick={() => setMenu((prev) => !prev)}>
+//                 <span>Menu</span>
+//                 {/* <MenuIcon>M</MenuIcon> */}
+//             </MenuButton>
+//         </li>
+//     );
+// };
 
 export const Header = () => {
-    const { theme, themes, setTheme } = useTheme();
-    const { session } = useSession();
+    // const { theme, themes, setTheme } = useTheme();
+    // const { session } = useSession();
 
-    const [search, setSearch] = useState(false);
+    // const [search, setSearch] = useState(false);
 
-    const refSearch = useRef<HTMLLIElement>(null);
-    useOnClickOutside(refSearch, () => setSearch(false));
+    // const refSearch = useRef<HTMLLIElement>(null);
+    // useOnClickOutside(refSearch, () => setSearch(false));
 
     return (
         <header className={styles.app_header}>
@@ -165,7 +166,7 @@ export const Header = () => {
                 </li>
             </ul> */}
 
-            <ul>
+            <ul style={{ padding: 0 }}>
                 <li data-menu="text">
                     <NextLink href="/">
                         <a>
@@ -216,7 +217,7 @@ export const Header = () => {
                 </li> */}
             </ul>
 
-            <ul>
+            {/* <ul>
                 <li data-menu="text">
                     <NextLink href="/blog">
                         <a>
@@ -244,9 +245,6 @@ export const Header = () => {
                         </a>
                     </NextLink>
                 </li>
-            </ul>
-
-            <ul>
                 {!session ? (
                     <li data-menu="text">
                         <NextLink href="/auth?form=sign-in">
@@ -264,9 +262,9 @@ export const Header = () => {
                         </NextLink>
                     </li>
                 )}
-            </ul>
+            </ul> */}
 
-            <ul>
+            {/* <ul>
                 <li>
                     <select
                         value={theme}
@@ -280,7 +278,7 @@ export const Header = () => {
                         ))}
                     </select>
                 </li>
-            </ul>
+            </ul> */}
         </header>
     );
 };
