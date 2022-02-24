12107c4420ebb44293e54fa36ae2ba09b34faede
Taufik Oktama <oktamataufik@gmail.com>
2022-02-24 21:42:33 +0700

    `feat` Improve lightbox

--- components/LightBox.tsx
+++ components/LightBox.tsx
@@ -1,6 +1,6 @@
 import styles from "styles/modal.module.scss";
 import type { CSSProperties, FC } from "react";
-import { useEffect, useRef } from "react";
+import { useRef } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 import NextHead from "next/head";
 import useLightBox from "hooks/use-light-box";
@@ -14,71 +14,6 @@ type BaseProps = {
 
 type LightBoxProps = BaseProps & {
     style?: CSSProperties;
-};
-
-type LightBoxHeaderProps = BaseProps & {};
-
-const LightBoxHeader = (props: LightBoxHeaderProps) => {
-    const { onRequestClose } = props;
-    return (
-        <header
-            style={{
-                position: "sticky",
-                top: 0,
-                zIndex: 1,
-                width: "100%",
-                margin: "0 auto",
-                pointerEvents: "initial"
-            }}
-        >
-            <div style={{ height: "var(--header-height)" }}>
-                <button
-                    onClick={onRequestClose}
-                    style={{
-                        appearance: "none",
-                        background: "none",
-                        border: "1px solid",
-                        borderRadius: "calc(var(--grid-gap) / 3)",
-                        padding: 0,
-                        margin: 0,
-                        fontFamily: "inherit",
-                        fontSize: "inherit",
-                        fontWeight: "bold",
-                        cursor: "pointer",
-                        aspectRatio: "1/1",
-                        width: "1.4em",
-                        backgroundColor: "var(--accents-1)",
-                        color: "var(--accents-12)",
-                        display: "flex",
-                        alignItems: "center",
-                        justifyContent: "center",
-                        position: "absolute",
-                        left: 0,
-                        top: "var(--grid-gap)"
-                    }}
-                >
-                    <span
-                        style={{
-                            fontSize: "0.8em",
-                            fontWeight: "bold",
-                            display: "inline-flex",
-                            alignItems: "center"
-                        }}
-                    >
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            height="1.5em"
-                            width="1.5em"
-                            viewBox="0 0 24 24"
-                            fill="currentColor"
-                        >
-                            <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z" />
-                        </svg>
-                    </span>
-                </button>
-            </div>
-        </header>
-    );
 };
 
 export const LightBox: FC<LightBoxProps> = (props) => {
@@ -101,6 +36,8 @@ export const LightBox: FC<LightBoxProps> = (props) => {
 
                     <motion.div
                         ref={refParent}
+                        className={styles.container}
+                        data-active={state}
                         initial={{ opacity: 0 }}
                         animate={{
                             opacity: 1,
@@ -110,34 +47,71 @@ export const LightBox: FC<LightBoxProps> = (props) => {
                             opacity: 0,
                             transition: { type: "spring", mass: 0.5, damping: 200, stiffness: 2000 }
                         }}
-                        className={styles.container}
                     >
                         <div
-                            onClick={onRequestClose}
                             style={{
-                                position: "fixed",
-                                inset: 0,
-                                cursor: "zoom-out"
+                                display: "inline-flex",
+                                alignItems: "center",
+                                position: "sticky",
+                                top: 0,
+                                height: "var(--header-height)",
+                                paddingInline: "1.5em"
                             }}
-                        />
+                        >
+                            <button
+                                name="Exit"
+                                title="Exit"
+                                style={{
+                                    appearance: "none",
+                                    background: "none",
+                                    border: "1px solid",
+                                    borderRadius: "100%",
+                                    padding: 0,
+                                    margin: 0,
+                                    fontFamily: "inherit",
+                                    fontSize: "inherit",
+                                    fontWeight: "bold",
+                                    cursor: "pointer",
+                                    aspectRatio: "1/1",
+                                    width: "1.5em",
+                                    backgroundColor: "var(--accents-1)",
+                                    color: "var(--accents-12)",
+                                    display: "flex",
+                                    alignItems: "center",
+                                    justifyContent: "center"
+                                }}
+                            >
+                                <span
+                                    style={{
+                                        display: "inline-flex",
+                                        alignItems: "center",
+                                        width: "100%",
+                                        height: "100%"
+                                    }}
+                                >
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        height="24px"
+                                        width="24px"
+                                        viewBox="0 0 24 24"
+                                        fill="currentColor"
+                                    >
+                                        <path d="M0 0h24v24H0V0z" fill="none" />
+                                        <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z" />
+                                    </svg>
+                                </span>
+                            </button>
+                        </div>
 
                         <div
                             style={{
                                 pointerEvents: "none",
                                 position: "relative",
-                                // padding: "0 0 calc(var(--grid-gap) * 3) 0",
-                                padding: 0,
+                                paddingBottom: "calc(var(--grid-gap) * 3)",
                                 paddingInline: "calc(var(--grid-gap) * 3)"
                             }}
                         >
-                            <LightBoxHeader title={title} onRequestClose={onRequestClose} />
-
-                            <div
-                                ref={refContent}
-                                className={styles.content_box}
-                                // data-layout="fluid"
-                                style={{ ...style }}
-                            >
+                            <div ref={refContent} className={styles.content_box} style={style}>
                                 {children}
                             </div>
                         </div>
