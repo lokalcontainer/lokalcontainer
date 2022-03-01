75f780998e34984c03701877671b7f11b80814a6
Taufik Oktama <oktamataufik@gmail.com>
2022-03-01 11:13:46 +0700

    `feat` Add `imageSizes`, `formats`, and `TTL`

--- next.config.js
+++ next.config.js
@@ -3,6 +3,13 @@ const API_URL = process.env.NEXT_PUBLIC_API_URL;
 module.exports = {
     reactStrictMode: true,
     poweredByHeader: false,
+    images: {
+        imageSizes: [
+            16, 32, 48, 64, 128, 256, 384, 512, 720, 960, 1280, 1366, 1536, 1920, 2200, 2560
+        ],
+        formats: ["image/avif", "image/webp"],
+        minimumCacheTTL: 60
+    },
     async rewrites() {
         return [
             { source: "/api/v1/:path*", destination: `${API_URL}/v1/:path*` },
