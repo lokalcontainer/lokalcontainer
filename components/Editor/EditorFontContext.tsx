76ff4d8a05a024cfdce8516122af8268f855d60e
Taufik Oktama <oktamataufik@gmail.com>
2022-03-04 12:02:27 +0700

    `feat` Create `handleSubmit()`

--- components/Editor/EditorFontContext.tsx
+++ components/Editor/EditorFontContext.tsx
@@ -1,5 +1,6 @@
-import type { Dispatch, FC, SetStateAction } from "react";
-import { createContext, useContext, useState } from "react";
+import type { Dispatch, SetStateAction } from "react";
+import { createContext, FC, useCallback, useContext, useState } from "react";
+import fetchJson from "libs/lib.fetch";
 import { Typeface } from "components/Editor/typetools";
 
 type ContextEditorFontProps = {
@@ -7,13 +8,15 @@ type ContextEditorFontProps = {
     setFiles: Dispatch<SetStateAction<File[]>>;
     typefaces: Typeface[];
     setTypefaces: Dispatch<SetStateAction<Typeface[]>>;
+    handleSubmit(): void;
 };
 
 const initState: ContextEditorFontProps = {
     files: [],
     setFiles: () => ({}),
     typefaces: [],
-    setTypefaces: () => ({})
+    setTypefaces: () => ({}),
+    handleSubmit: () => ({})
 };
 
 const ContextEditorFont = createContext<ContextEditorFontProps>(initState);
@@ -24,8 +27,33 @@ const ProviderEditorFont: FC = (props) => {
     const [files, setFiles] = useState<File[]>([]);
     const [typefaces, setTypefaces] = useState<Typeface[]>([]);
 
+    const handleSubmit = useCallback(() => {
+        if (typefaces.length === 0) return;
+
+        const selected = typefaces[0];
+        const identifier = selected.subFamily ? selected.subFamily : selected.family;
+        const newSlug = identifier.replace(/\s/g, "-").toLowerCase();
+
+        const buildPost = {
+            title: identifier,
+            slug: newSlug,
+            type: "font",
+            typefaces
+        };
+
+        fetchJson(`/api/v1/posts/${newSlug}`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(buildPost)
+        })
+            .then((res) => console.log(res))
+            .catch((err) => console.log(err));
+    }, [typefaces, files]);
+
     return (
-        <ContextEditorFont.Provider value={{ files, setFiles, typefaces, setTypefaces }}>
+        <ContextEditorFont.Provider
+            value={{ files, setFiles, typefaces, setTypefaces, handleSubmit }}
+        >
             {children}
         </ContextEditorFont.Provider>
     );
