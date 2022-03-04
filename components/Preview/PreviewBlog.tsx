e95359f9859188d7b599c71ef6a15a3769953995
Taufik Oktama <oktamataufik@gmail.com>
2022-03-05 00:02:11 +0700

    `fix` Escape `'` using `&apos;`

--- components/Preview/PreviewBlog.tsx
+++ components/Preview/PreviewBlog.tsx
@@ -359,7 +359,7 @@ export default function PreviewBlog(props: PreviewBlogProps) {
                         <figcaption>
                             There are many variations of passages of Lorem Ipsum available, but the
                             majority have suffered alteration in some form, by injected humour, or
-                            randomised words which don't look even slightly believable.
+                            randomised words which don&apos;t look even slightly believable.
                         </figcaption>
                     </figure>
 
