340213bf10a0309c1e1b43998b179a0f53e3c471
Taufik Oktama <oktamataufik@gmail.com>
2022-03-21 12:30:18 +0700

    `fix` Direct children to avoid blank space

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,9 +1,9 @@
 import styles from "styles/footer.module.scss";
 import type { CSSProperties } from "react";
-import { Fragment } from "react";
 import { useRouter } from "next/router";
 import NextDynamic from "next/dynamic";
 import NextLink from "next/link";
+import { useWindow } from "components/Context/ContextWindow";
 import Marquee from "components/Utils/Marquee";
 import ThemeSelector from "components/Utils/ThemeSelector";
 
@@ -44,109 +44,196 @@ export default function Footer() {
     const currentYear = date.getFullYear();
 
     const { pathname } = useRouter();
+    const { focus } = useWindow();
 
     return (
         <footer className={styles.app_footer} data-page={pathname.split("/")[1]}>
-            <Marquee pauseOnHover speed={24} style={{ fontFeatureSettings: `"tnum", "case"` }}>
-                {Array(3)
-                    .fill(
-                        <>
-                            <div style={{ marginRight: "2em" }}>
-                                <div style={{ fontSize: "0.75em" }}>
-                                    &copy;{currentYear} Lokal Container with Love. Find us at{" "}
-                                    <a
-                                        style={aStyle}
-                                        href="https://goo.gl/maps/hbGUeAxtG92uZ5i39"
-                                        target="_blank"
-                                        rel="noopener noreferrer"
-                                    >
-                                        Lou Belle Space
-                                    </a>{" "}
-                                    or follow us on{" "}
-                                    <a
-                                        style={aStyle}
-                                        href="https://instagram.com/lokalcontainer"
-                                        target="_blank"
-                                        rel="noopener noreferrer"
-                                    >
-                                        Instagram
-                                    </a>
-                                    ,{" "}
-                                    <a
-                                        style={aStyle}
-                                        href="https://github.com/lokalcontainer"
-                                        target="_blank"
-                                        rel="noopener noreferrer"
-                                    >
-                                        Github
-                                    </a>
-                                    , and{" "}
-                                    <a
-                                        style={aStyle}
-                                        href="https://twitter.com/lokalcontainer"
-                                        target="_blank"
-                                        rel="noopener noreferrer"
-                                    >
-                                        Twitter
-                                    </a>
-                                    .
-                                </div>
-                            </div>
+            <Marquee
+                play={focus}
+                pauseOnHover
+                speed={30}
+                delay={5}
+                style={{ fontFeatureSettings: `"tnum", "case"` }}
+            >
+                <div style={{ marginRight: "2em" }}>
+                    <div style={{ fontSize: "0.75em" }}>
+                        &copy;{currentYear} Lokal Container with Love. Find us at{" "}
+                        <a
+                            style={aStyle}
+                            href="https://goo.gl/maps/hbGUeAxtG92uZ5i39"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            Lou Belle Space
+                        </a>{" "}
+                        or follow us on{" "}
+                        <a
+                            style={aStyle}
+                            href="https://instagram.com/lokalcontainer"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            Instagram
+                        </a>
+                        ,{" "}
+                        <a
+                            style={aStyle}
+                            href="https://github.com/lokalcontainer"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            Github
+                        </a>
+                        , and{" "}
+                        <a
+                            style={aStyle}
+                            href="https://twitter.com/lokalcontainer"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            Twitter
+                        </a>
+                        .
+                    </div>
+                </div>
 
-                            <div style={{ marginRight: "2em" }}>
-                                <ul
-                                    style={{
-                                        fontSize: "0.75em",
-                                        listStyle: "none",
-                                        padding: 0,
-                                        margin: 0,
-                                        display: "flex",
-                                        alignItems: "center",
-                                        gap: "calc(var(--grid-gap) * 1)"
-                                    }}
-                                >
-                                    {staticLinks.map((item, i) => (
-                                        <li key={i}>
-                                            <NextLink href={item.href}>
-                                                <a
-                                                    style={{
-                                                        ...aStyle,
-                                                        color: "var(--accents-12)"
-                                                    }}
-                                                >
-                                                    {item.label}
-                                                </a>
-                                            </NextLink>
-                                        </li>
-                                    ))}
-                                </ul>
-                            </div>
+                <div style={{ marginRight: "2em" }}>
+                    <ul
+                        style={{
+                            fontSize: "0.75em",
+                            listStyle: "none",
+                            padding: 0,
+                            margin: 0,
+                            display: "flex",
+                            alignItems: "center",
+                            gap: "calc(var(--grid-gap) * 1)"
+                        }}
+                    >
+                        {staticLinks.map((item, i) => (
+                            <li key={i}>
+                                <NextLink href={item.href}>
+                                    <a
+                                        style={{
+                                            ...aStyle,
+                                            color: "var(--accents-12)"
+                                        }}
+                                    >
+                                        {item.label}
+                                    </a>
+                                </NextLink>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
 
-                            <div
-                                style={{
-                                    marginRight: "2em",
-                                    display: "inline-flex",
-                                    alignItems: "center",
-                                    gap: "calc(var(--grid-gap) / 2)"
-                                }}
-                            >
-                                <Measurment
-                                    style={{
-                                        fontSize: "0.625em",
-                                        borderRadius: "2em",
-                                        padding: "0.15em 0.75em",
-                                        backgroundColor: "var(--accents-3)"
-                                    }}
-                                />
-                                <ThemeSelector
-                                    style={{ fontSize: "0.625em", textTransform: "uppercase" }}
-                                />
-                            </div>
-                        </>
-                    )
-                    .map((item, i) => (
-                        <Fragment key={i}>{item}</Fragment>
-                    ))}
+                <div
+                    style={{
+                        marginRight: "2em",
+                        display: "inline-flex",
+                        alignItems: "center",
+                        gap: "calc(var(--grid-gap) / 2)"
+                    }}
+                >
+                    <Measurment
+                        style={{
+                            fontSize: "0.625em",
+                            borderRadius: "2em",
+                            padding: "0.15em 0.75em",
+                            backgroundColor: "var(--accents-3)"
+                        }}
+                    />
+                    <ThemeSelector style={{ fontSize: "0.625em", textTransform: "uppercase" }} />
+                </div>
+
+                <div style={{ marginRight: "2em" }}>
+                    <div style={{ fontSize: "0.75em" }}>
+                        &copy;{currentYear} Lokal Container with Love. Find us at{" "}
+                        <a
+                            style={aStyle}
+                            href="https://goo.gl/maps/hbGUeAxtG92uZ5i39"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            Lou Belle Space
+                        </a>{" "}
+                        or follow us on{" "}
+                        <a
+                            style={aStyle}
+                            href="https://instagram.com/lokalcontainer"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            Instagram
+                        </a>
+                        ,{" "}
+                        <a
+                            style={aStyle}
+                            href="https://github.com/lokalcontainer"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            Github
+                        </a>
+                        , and{" "}
+                        <a
+                            style={aStyle}
+                            href="https://twitter.com/lokalcontainer"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            Twitter
+                        </a>
+                        .
+                    </div>
+                </div>
+
+                <div style={{ marginRight: "2em" }}>
+                    <ul
+                        style={{
+                            fontSize: "0.75em",
+                            listStyle: "none",
+                            padding: 0,
+                            margin: 0,
+                            display: "flex",
+                            alignItems: "center",
+                            gap: "calc(var(--grid-gap) * 1)"
+                        }}
+                    >
+                        {staticLinks.map((item, i) => (
+                            <li key={i}>
+                                <NextLink href={item.href}>
+                                    <a
+                                        style={{
+                                            ...aStyle,
+                                            color: "var(--accents-12)"
+                                        }}
+                                    >
+                                        {item.label}
+                                    </a>
+                                </NextLink>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+
+                <div
+                    style={{
+                        marginRight: "2em",
+                        display: "inline-flex",
+                        alignItems: "center",
+                        gap: "calc(var(--grid-gap) / 2)"
+                    }}
+                >
+                    <Measurment
+                        style={{
+                            fontSize: "0.625em",
+                            borderRadius: "2em",
+                            padding: "0.15em 0.75em",
+                            backgroundColor: "var(--accents-3)"
+                        }}
+                    />
+                    <ThemeSelector style={{ fontSize: "0.625em", textTransform: "uppercase" }} />
+                </div>
             </Marquee>
         </footer>
     );
