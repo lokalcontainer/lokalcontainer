ccc73b3ea5ef30cb21ffc6e7cbde9234b9d10b43
Taufik Oktama <oktamataufik@gmail.com>
2022-03-23 23:16:50 +0700

    `feat` Modified context

--- components/Context/ContextFont.tsx
+++ components/Context/ContextFont.tsx
@@ -1,45 +1,32 @@
-import { createContext, FC, useContext, useEffect, useState } from "react";
-import { fonts, FontType, typefaces, TypefaceType } from "libs/fonts.dummy";
+import type { PropsWithChildren } from "react";
+import { createContext, useContext } from "react";
+import { fonts, FontType } from "libs/fonts.dummy";
 
 type ContextFontProps = {
-    selectedTypeface: string;
-    changeTypeface: (_e: string) => void;
-    typefaces: TypefaceType[];
+    slug: string;
     font: FontType;
 };
 
-type ProviderFontProps = {
+type ProviderFontProps = PropsWithChildren<{
     font?: FontType;
-};
+    slug: string;
+}>;
 
 const ContextFont = createContext<ContextFontProps>(undefined!);
 export const useFont = () => useContext(ContextFont);
 
 export const ConsumerFont = ContextFont.Consumer;
 
-export const ProviderFont: FC<ProviderFontProps> = (props) => {
-    const { children, font } = props;
-    const filteredTypefaces = font?.typefacesID?.map(
-        (parent) => typefaces.find((item) => item.id === parent)!
-    )!;
-    const [selectedTypeface, setSelectedTypeface] = useState(filteredTypefaces[0]?.fullName);
-
-    const changeTypeface = (fullName: string) => setSelectedTypeface(fullName);
-
-    useEffect(() => {
-        changeTypeface(filteredTypefaces[0].fullName);
-    }, [font, filteredTypefaces]);
-
+export default function ProviderFont(props: ProviderFontProps) {
+    const { children, font, slug } = props;
     return (
         <ContextFont.Provider
             value={{
-                selectedTypeface,
-                changeTypeface,
-                typefaces: filteredTypefaces,
+                slug: slug,
                 font: font ?? fonts[0]
             }}
         >
             {children}
         </ContextFont.Provider>
     );
-};
+}
